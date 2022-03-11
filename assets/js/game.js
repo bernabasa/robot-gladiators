e2eeefc8d71c210b55a3eca9102d7e08c9e0a5c5
Your Name <bernabas.abdeta@yahoo.com>
2022-03-11 04:22:00 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,12 +8,13 @@ console.log(playerName, playerAttack, playerHealth);
 
 var enemyName = "Roborto";
 var enemyHealth = 50;
-var enemyAttack = 12;
+var enemyAttack = 10;
 
 var fight = function() {
-  window.alert("Welcome to Robot Gladiators!");
-  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-  
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+var promptFight = window.prompt (" Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
 // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
   // remove enemy's health by subtracting the amount set in the playerAttack variable
@@ -38,19 +39,23 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   // check player's health
   if (playerHealth <= 0) {
     window.alert(playerName + " has died!");
-  } else if (promptFight === "skip" || promptFight === "SKIP") {
-    // confirm player wants to skip
-    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-  
-    // if yes (true), leave fight
-    if (confirmSkip) {
-      window.alert(playerName + " has decided to skip this fight. Goodbye!");
-      // subtract money from playerMoney for skipping
-      playerMoney = playerMoney - 2;
-    }
-    // if no (false), ask question again by running fight() again
-    else {
-      fight();
-    }
+  } else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
   }
-fight();
+  // if player choses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+  window.alert(playerName + " has chosen to skip the fight!");
+var confirmSkip = window.confirm ("Are you sure you'd like to skip?");
+
+if (confirmSkip) {
+  window.alert(playerName + "has descided to skip this fight. Goodbaye!");
+
+}
+
+
+} else {
+  fight();
+}
+};
+ 
+  fight();
